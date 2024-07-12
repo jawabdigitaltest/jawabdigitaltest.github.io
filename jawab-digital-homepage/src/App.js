bef92cac78a90d8ed7aa1af828c8998b6df7c195
joelwjx <35244327+joelwjx@users.noreply.github.com>
2024-07-12 18:19:07 +0800

    initial commit

---
+++ jawab-digital-homepage/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./Home";
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div>
+				<Home />
+			</div>
+		</div>
+	);
+}
+
+export default App;
